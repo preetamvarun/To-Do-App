e5d646869a9adc58b499d99b79c7fe76c84af4e1
Preetam Varun <preetamarun99@gmail.com>
2021-08-12 14:11:35 +0530

    add icons with a circle avatar, add Todoey and 12 tasks and style them

--- lib/screens/screen1.dart
+++ lib/screens/screen1.dart
@@ -7,6 +7,40 @@ class TaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
+      
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+            backgroundColor: Colors.white,
+              radius: 30.0,
+              child: Icon(
+                Icons.analytics_outlined,
+                size: 30.0,
+              ),
+            ),
+            SizedBox(
+              height : 5.0,
+            ),
+            Text(
+              "Todoey",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+                fontSize: 40,
+              ),
+            ),
+            Text(
+              "12 Tasks",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
