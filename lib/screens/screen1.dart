2cf022722554834948bef86bfd13fc7dfab6986d
Preetam Varun <preetamarun99@gmail.com>
2021-08-12 17:44:59 +0530

    add ListView and keep some widgets with checkboxes

--- lib/screens/screen1.dart
+++ lib/screens/screen1.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TaskScreen extends StatelessWidget {
@@ -7,39 +8,73 @@ class TaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      
-      body: Padding(
-        padding: const EdgeInsets.all(50.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CircleAvatar(
-            backgroundColor: Colors.white,
-              radius: 30.0,
-              child: Icon(
-                Icons.analytics_outlined,
-                size: 30.0,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(30, 60, 30, 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(
+                    Icons.analytics_outlined,
+                    size: 30.0,
+                  ),
+                ),
+                SizedBox(
+                  height : 5.0,
+                ),
+                Text(
+                  "Todoey",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 60,
+                  ),
+                ),
+                Text(
+                  "12 Tasks",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: ListView(
+                children: [
+                  ListTile(
+                    leading : Text("Buy Butter Milk", style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 18.0,
+                    ),),
+                    trailing : Icon(Icons.check_box_outline_blank),
+                  ),
+                  ListTile(
+                    leading : Text("Develop Flutter Projects",style:TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 18.0,
+                    ),),
+                    trailing : Icon(Icons.check_box_outline_blank),
+                  ),
+                ],
               ),
-            ),
-            SizedBox(
-              height : 5.0,
-            ),
-            Text(
-              "Todoey",
-              style: TextStyle(
+              decoration: BoxDecoration(
                 color: Colors.white,
-                fontWeight: FontWeight.w700,
-                fontSize: 40,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
               ),
             ),
-            Text(
-              "12 Tasks",
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
